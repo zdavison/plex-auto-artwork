cdf8bdd3d63f8b21721c625420299ce513c058ec
thingsdoer <thingsdoer@feralhosting.com>
2017-04-25 13:23:07 +0000

    Escaped globs now handled correctly, additional logging added

--- plex.rb
+++ plex.rb
@@ -16,26 +16,29 @@ end
 Dir.chdir(DIR)
 folders = Dir.glob('**/*').select {|f| File.directory? f}
 
-puts timestamp() + ">Beginning plex-auto-artwork.."
+puts timestamp() + ">>> Beginning plex-auto-artwork.."
 
 folders.each do |folder|
-
-	mp3_glob 				= escape_glob(folder + '/*.{mp3,flac}')
-	background_glob	= escape_glob(folder + '/' + FILENAME_BACKGROUND + ".{jpg,jpeg}")
-	poster_glob			= escape_glob(folder + '/' + FILENAME_POSTER + ".{jpg,jpeg}")
+	
+	escaped_folder = escape_glob(folder)
+	mp3_glob 		= escaped_folder + '/*.{mp3,flac}'
+	background_glob		= escaped_folder + '/' + FILENAME_BACKGROUND + ".{jpg,jpeg}"
+	poster_glob		= escaped_folder + '/' + FILENAME_POSTER + ".{jpg,jpeg}"
 
   contains_music      = Dir.glob(mp3_glob).count > 0
   contains_background = Dir.glob(background_glob).count > 0
   contains_poster     = Dir.glob(poster_glob).count > 0
 
+	puts "Inspecting> " + folder + " |#{contains_music}|#{contains_background}|#{contains_poster}|"
+
   if contains_music then
-    largest = Dir.glob(folder + '/*.{jpg,jpeg}').sort_by{ |image| File.size?(image) }.last
+    largest = Dir.glob(escaped_folder + '/*.{jpg,jpeg}').sort_by{ |image| File.size?(image) }.last
     FileUtils.cp(File.path(largest), File.dirname(largest) + "/" + FILENAME_BACKGROUND + File.extname(largest)) unless contains_background
     FileUtils.cp(File.path(largest), File.dirname(largest) + "/" + FILENAME_POSTER + File.extname(largest)) unless contains_poster
     
-    puts timestamp() + ">Created background in '#{folder}'" unless contains_background
-    puts timestamp() + ">Created poster in '#{folder}'"     unless contains_poster
+    puts timestamp() + "> Created background in '#{folder}'" unless contains_background
+    puts timestamp() + "> Created poster in '#{folder}'"     unless contains_poster
   end
 end
 
-puts timestamp() + ">Ending plex-auto-artwork.."
+puts timestamp() + "<<< Ending plex-auto-artwork.."
